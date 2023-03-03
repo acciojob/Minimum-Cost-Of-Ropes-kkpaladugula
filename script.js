8894997454e27e7011985a92029a4759b11f66b5
aami_mm <mukarramos@outlook.com>
2023-03-03 11:25:18 +0530

    Completed Code

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
+function calculateMinCost() 
+{
+      var firstinput=document.querySelector("input").value;
+      var arr=firstinput.split(",");
+
+      arr.sort(function (a, b) 
+      {
+      return a - b;
+      });
+
+    var cost=0;
+    while(arr.length>1)
+    {
+        var result=Number(arr[0])+Number(arr[1]);
+        arr.splice(0,2);
+        arr.push(result);
+        cost+=result;
+
+        arr.sort(function(a,b)
+        {
+          return a-b;
+        });
+    }
+
+
+    let result1 = document.getElementById("result");	
+    result1.textContent=cost
+    result1.classList.add("div");
 }  
